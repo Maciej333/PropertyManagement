7d222edfb24046447f885fd4e6c586d2b1f2abaf
Maciej Rogowski <maciej.rogowski333@gmail.com>
2020-10-02 12:02:03 +0200

    init commit

---
+++ src/main/java/rogowski/maciej/property/management/Application.java
@@ -0,0 +1,13 @@
+package rogowski.maciej.property.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
