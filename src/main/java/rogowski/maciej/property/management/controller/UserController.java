5c2865ddd55c3abcce098acaa9bbc9376fc7b743
Maciej Rogowski <maciej.rogowski333@gmail.com>
2020-10-05 18:25:22 +0200

    add user password change, enable user to change password

--- src/main/java/rogowski/maciej/property/management/controller/UserController.java
+++ src/main/java/rogowski/maciej/property/management/controller/UserController.java
@@ -2,6 +2,7 @@ package rogowski.maciej.property.management.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import rogowski.maciej.property.management.Service.UserService;
 import rogowski.maciej.property.management.entity.Property;
 import rogowski.maciej.property.management.entity.User;
+import rogowski.maciej.property.management.entity.userPasswordModel;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
 	private UserService userService;
+	private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	@Autowired
 	public UserController(UserService theUserService) {
@@ -29,18 +32,36 @@ public class UserController {
 	public String showUser(Authentication authentication, Model theModel){
 		User theUser = userService.findById(authentication.getName());
 		theModel.addAttribute("user", theUser);			
-		theModel.addAttribute("user1", theUser);		
 		Property theProperty = theUser.getProperty();
-		theModel.addAttribute("property", theProperty);
-		
+		theModel.addAttribute("property", theProperty);	
+		theModel.addAttribute("userPasswordModel", new userPasswordModel());
 		return "/user/user";
 	}
 
 	@PostMapping("/userUpdate")
-	public String updateUser(@ModelAttribute("user1") User theUser) {		
+	public String updateUser(@ModelAttribute("user") User theUser) {		
 		theUser.setPassword(userService.findById(theUser.getLogin()).getPassword());
 		theUser.setProperty(userService.findById(theUser.getLogin()).getProperty());
 		userService.save(theUser);
+		return "redirect:/user/user";
+	}
+	
+	@PostMapping("/userPassword")
+	public String changeUserPassword(@ModelAttribute("userPasswordModel") userPasswordModel theUserPasswordModel, @ModelAttribute("user") User theUser) {		
+		theUser = userService.findById(theUser.getLogin());
+		String oldPassword = theUserPasswordModel.getOldPassword();
+		String rawPassword = theUserPasswordModel.getNewPassword();
+		String confirmRawPassword = theUserPasswordModel.getConfirmNewPassword();
+		if(rawPassword.equals(confirmRawPassword)) {
+			if( passwordEncoder.matches(oldPassword, theUser.getPassword().substring(8, 68)             ) ) {			
+				theUser.setPassword("{bcrypt}"+passwordEncoder.encode(rawPassword));
+				userService.save(theUser);
+			}else {
+				
+			}
+		}else {
+
+		}
 		return "redirect:/user/user";
 	}
 }
--- src/main/resources/static/js/userBtnAction.js
+++ src/main/resources/static/js/userBtnAction.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
     $("#userEditInfo").click(function(){
         $(".userInfo").hide();
         $(".userEdit").show();
+        $(".userPassword").hide();
+        $(".userOnButton").hide();
+    });
+    
+    $("#userChangePassword").click(function(){
+        $(".userInfo").hide();
+        $(".userEdit").hide();
+        $(".userPassword").show();
         $(".userOnButton").hide();
     });
 });
